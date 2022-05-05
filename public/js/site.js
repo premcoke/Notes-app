a3b97c2db70adf149bdc906d96f6f7323be77c1c
Prem Kumar <kprem886@gmail.com>
2022-05-05 23:35:50 +0530

    Update site.js

--- public/js/site.js
+++ public/js/site.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', async function (e) {
     const cityValue = e.currentTarget.querySelector('input').value;
     const jsonEle = document.querySelector('#json');
     e.preventDefault();
-    const url = `http://localhost:3000/weather?address=${cityValue}`;
+    const url = `/weather?address=${cityValue}`;
     const dataValue = await fetch(url).then((response) => response.json()).then((data) => { return data }).catch((err) => error = err);
     jsonEle.innerHTML = dataValue.error == null ? dataValue.body.current.condition.text : dataValue.error;
 });
